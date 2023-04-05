48aaa28485be3e1541e067da6e384bf8a607aff2
ooooMaXoooo <114589773+ooooMaXoooo@users.noreply.github.com>
2023-04-05 09:58:23 +0200

    Add files via upload

--- Escape.py
+++ Escape.py
@@ -18,16 +18,19 @@ class Player :     # sa position dans le monde s'affichera dans la loop
         self.onGround = True
         self.jumpHeight = 50
         self.jumpPower = 10
-        self.path = './image/sprite/Augur_Ambros/'
+        self.path = 'sprite/Augur_Ambros/'
 
     def move(self, direction):
-        print(f"posX : {self.posX}\nposY : {self.posY}")
+        print(f"[Player::move()] INFO:\tposX : {self.posX}\n[Player::move()] INFO:\tposY : {self.posY}")
         if self.posX >= largeur * TILE_SIZE or self.posX < 1:
-            print("[Player::actualizePos()] INFO:\t " + str("Can't go there"))
+            print("[Player::move()] INFO:\t" + "Can't go there")
             if self.posX >= largeur * TILE_SIZE :
                 self.posX -= 5
+                print("[Player::move()] INFO:\t" + "player is at the right border")
+                print("[player::move()] INFO:\t" + f"posX : {self.posX}\t\t\tposY : {self.posY}")
             elif self.posX < 1:
                 self.posX += 5
+                print("[Player::move()] INFO:\t" + "player is at the left border")
 
         autorized = not isInside((self.posX,self.posY), self.posLvl)
 
@@ -104,7 +107,7 @@ def isInside(pos1, worldPos):  # on doit test avec tous les objets de la scène 
 
 
 def tp(position, object):
-    print("tp player")
+    print("[TP()]INFO :\ttp player")
 
 
 #########################################################################
@@ -123,7 +126,7 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 
 
 #variables de la jungle
-NB_TILES = 20   #nombre de tiles a charger (ici de 00.png à 19.png) 20 au total
+NB_TILES = 21   #nombre de tiles a charger (ici de 00.png à 19.png) 20 au total
 TILE_SIZE=64   #definition du dessin (carré) taille de la taille en px
 
 largeur=8       #hauteur du niveau
@@ -132,7 +135,6 @@ tiles=[]       #liste d'images tiles
 
 #variables de gestion du joueur
 player = Player((1,5),32,32,5, 5)
-compteurBilles=0
 
 #variables de gestion du fantome
 FRAMERATE_FANTOME= 120      #vitesse du fantome chiffre elevé = vitesse lente
@@ -146,23 +148,15 @@ directionF = 1
 #definition du niveau les nb coressponde aux nb des tiles
 
 niveau = [
-     [-1, -1, -1, -1, -1, -1, -1, -1],
+     [20, 20, -1, -1, -1, -1, -1, -1],
      [8, -1, -1, -1, -1, -1, -1, -1],
      [8, -1, -1, -1, -1, -1, -1, -1],
      [8, -1, -1, -1, -1, -1, -1, -1],
      [8, -1, -1, 3, 4, -1, -1, -1],
      [13, 12, 12, 18, 19, 11, 12, 4]]
 
-fantome=[
-     [0,0,0,0,0,0,0,0],
-     [0,0,0,4,0,0,0,0],
-     [0,0,0,3,0,0,0,0],
-     [0,0,0,2,0,0,0,0],
-     [0,0,0,1,0,0,0,0],
-     [0,0,0,0,0,0,0,0]]
 
-
-fenetre = pygame.display.set_mode((largeur*TILE_SIZE, (hauteur+1)*TILE_SIZE))
+fenetre = pygame.display.set_mode((largeur*TILE_SIZE, hauteur*TILE_SIZE), pygame.DOUBLEBUF, 32)
 
 def chargetiles(tiles):
     """
@@ -170,69 +164,27 @@ def chargetiles(tiles):
     pour l'instant, que dans jungle
     """
     for n in range(NB_TILES):
-        tiles.append(pygame.image.load(fr'image/map/Tilemaps/jungle/tile'+str(n)+'.png')) #attention au chemin
+        tiles.append(pygame.image.load(fr'Tilemaps/jungle/tile'+str(n)+'.png').convert_alpha()) #attention au chemin
         #   U:/Documents/NSI/Mini-Projets/jeuNoel/image/map/Tilemaps/jungle/tile
 
 def afficheNiveau(niveau):
     """
     affiche le niveau a partir de la liste a deux dimensions niveau[][]
     """
+    bck = pygame.image.load("backgrounds/jungle.png")
+    fenetre.blit(bck, (0,0))
     for y in range(hauteur): # changer ça plus tard pour afficher une partie de la map
         for x in range(largeur):
             fenetre.blit(tiles[niveau[y][x]],(x*TILE_SIZE,y*TILE_SIZE))
 
 
-def afficheJoueur(numero):
+def afficheJoueur():
     """
     affiche le joueur en position x et y
     """
     x= player.posX
     y = player.posY
     fenetre.blit(pygame.image.load(player.path + "droit1.png"),(x, y)) #  changer tt ça
-
-
-def afficheScore(score):
-    """
-    affiche le score
-    changer le système de score
-    """
-    scoreAafficher = font.render(str(score), True, (0, 255, 0))
-    fenetre.blit(scoreAafficher,(120,250))
-
-def rechercheFantome(fantome,position): #recherche les coord du fantome dans la liste fantome
-    """
-    recherche les coordonnées du fantome en fonction du numéro de sa postion dans le parcours
-    """
-    print(position)                     #la position doit etre dans la liste fantome sinon plantage
-    for y in range(hauteur):
-        for x in range(largeur):
-            if fantome[y][x]==position:
-                coodFantome=x,y
-    return coodFantome          #les coord du fantome x et y sont dans un tuple coodFantome
-
-def deplaceFantome(fantome):
-    """
-    Incrémente automatiquement le déplacement du fantome, gère sa vitesse et son affichage
-    """
-    global frameRateCounterFantome
-    global positionFantome
-    global posfX,posfY
-    global directionF
-    if frameRateCounterFantome==FRAMERATE_FANTOME:      #ralenti la viteese du fantome
-        posfX,posfY=rechercheFantome(fantome,positionFantome)   #deballage du tuple coordonnées du fantome
-        if positionFantome==NB_DEPLACEMENT_FANTOME:     #un tour est fait donc on passe à la 1ere position
-            directionF = -1
-        if positionFantome == 1:
-            directionF = 1
-
-        positionFantome += 1*directionF
-
-        frameRateCounterFantome=0                       #compteur de vitesse à zero
-    fenetre.blit(tiles[15],(posfX * TILE_SIZE,posfY * TILE_SIZE)) #affichage du fantome
-    frameRateCounterFantome+=1                          #incrémentation du compteur de vitesse
-
-
-
 
 
 
@@ -266,11 +218,11 @@ while not isDead:
 
 
     fenetre.fill((0,0,0))   #efface la fenetre
-    pygame.display.update() #mets à jour la fentre graphique
 
     afficheNiveau(niveau)   #affiche le niveau
-    afficheJoueur(14)          #affiche le joueur et le score
-    deplaceFantome(fantome) #mettre un commentaire pour desactiver le déplacement du fantome
-    afficheScore(compteurBilles)
+    afficheJoueur()          #affiche le joueur et le score
+
+    pygame.display.flip() #mets à jour la fentre graphique
+
 
 pygame.quit()
